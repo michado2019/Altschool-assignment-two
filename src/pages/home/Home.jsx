a5e46e79a1f370793effa99ac2f754f993631ce4
Michael Adeshina <adeshinaobafemi09@gmail.com>
2022-10-20 14:02:16 +0100

    welcome address part finished

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,22 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./Home.css";
 
 export default function Home() {
-  return <div>home</div>;
+  const [welcome, setWelcome] = React.useState("Hi, I am a student of Altschool Africa. Welcome to my site!");
+  const welcomeRef = useRef(null)
+
+  setTimeout(() => {
+    welcomeRef.current = 'To view users list, click on the "Users" list box below!';
+    const welcomeAddress = welcomeRef.current;
+    setWelcome(welcomeAddress);
+  }, 3000)
+  return (
+    <div className="homeWrapper">
+      <div className="homeWecome-address_div">
+        <h3 className="homeWecome-address">
+          {welcome}
+        </h3>
+      </div>
+    </div>
+  );
 }
